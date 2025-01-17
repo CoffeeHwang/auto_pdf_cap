e6939f9f317295d5477b68ceea59b894e3b11cc0
Coffee.Hwang <hgamma@naver.com>
2025-01-17 11:06:18 +0900

    refactor: BasicTab으로 캡처 영역 창 관련 로직 이동
    - MainWindow에서 BasicTab으로 캡처 영역 창 관련 로직 이동
      - create_cap_region_window, show_cap_region_window, hide_cap_region_window 메서드 추가
      - toggle_cap_region_window, clear_rectangles, change_opacity 메서드 이동
      - on_margin_changed, on_diff_width_changed 메서드 이동
    
    - UI 개선
      - margin과 diff_width 레이아웃을 QHBoxLayout으로 변경
      - placeholder 텍스트 추가
      - 툴팁 텍스트 명확화
    
    - 설정 관리 개선
      - 캡처 영역 창 설정을 BasicTab에서 관리
      - 투명도 관련 로직을 setOpacity 메서드로 통합
    
    이 변경으로 BasicTab과 MainWindow 사이의 결합도가 낮아지고
    각 클래스의 책임이 더 명확해졌습니다.

--- main_window.py
+++ main_window.py
@@ -3,8 +3,6 @@ from PyQt5.QtWidgets import (QMainWindow, QPushButton, QWidget,
                            QMenuBar, QMenu, QAction, QShortcut)
 from PyQt5.QtCore import Qt, QRect, QPoint, QThread
 from PyQt5.QtGui import QKeySequence
-from cap_region_window import CapRegionWindow
-from worker_cap import WorkerCapture
 from tab_basic import BasicTab
 from tab_ocr import OcrTab
 from tab_gen_outline import GenOutlineTab
@@ -16,9 +14,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.settings = SupaSettings()  
-        self.cap_region_window = None
         self.initUI()
-        self.create_cap_region_window()  # 시작 시 캡처 영역 창 생성만 하고 보이지 않게 함
         self.loadSettings()   # 설정 불러오기
         self.setup_shortcuts()
 
@@ -56,162 +52,21 @@ class MainWindow(QMainWindow):
         if geometry:
             self.restoreGeometry(geometry)
             
-        # LineEdit 값들 불러오기
-        self.basic_tab.margin_edit.setText(self.settings.value('MainWindow/margin', '0'))
-        self.basic_tab.diff_width_edit.setText(self.settings.value('MainWindow/diff_width', '0'))
-        
-        # 투명도 슬라이더 값 불러오기
-        opacity = int(self.settings.value('MainWindow/opacity', 50))
-        self.basic_tab.opacity_slider.setValue(opacity)
-        self.basic_tab.opacity_label.setText(f'투명도: {opacity}%')
-        
-        # 캡처 영역 창 설정 불러오기
-        if self.cap_region_window:
-            # 창 위치/크기 복원
-            cap_region_geometry = self.settings.value('CapRegionWindow/geometry')
-            if cap_region_geometry:
-                self.cap_region_window.restoreGeometry(cap_region_geometry)
-            
-            # 투명도 적용
-            self.cap_region_window.setWindowOpacity(opacity / 100.0)
-            
-            # 저장된 사각형이 있다면 복원
-            if self.settings.value('MainWindow/y1') or self.settings.value('MainWindow/x2') or self.settings.value('MainWindow/y2'):  # 하나라도 0이 아닌 값이 있다면
-                window_pos = self.cap_region_window.mapToGlobal(QPoint(0, 0))
-                rect = QRect(
-                    0 - window_pos.x(),
-                    int(self.settings.value('MainWindow/y1')) - window_pos.y(),
-                    int(self.settings.value('MainWindow/x2')) - 0,
-                    int(self.settings.value('MainWindow/y2')) - int(self.settings.value('MainWindow/y1'))
-                )
-                self.cap_region_window.rectangle = rect
-                self.cap_region_window.can_draw = False  # 새로 그리기 방지
-                self.cap_region_window.update()
-        
-        # 기타설정값 불러오기
-        self.basic_tab.file_name_edit.setText(self.settings.value('MainWindow/file_name', ''))
-        self.basic_tab.page_loop_edit.setText(self.settings.value('MainWindow/page_loop', ''))
-        self.basic_tab.delay_edit.setText(self.settings.value('MainWindow/capture_delay', '0'))
+        # BasicTab 설정 불러오기
+        self.basic_tab.loadSettings()
 
     def saveSettings(self):
         """현재 설정 저장"""
         # 창 위치/크기 저장
         self.settings.setValue('MainWindow/geometry', self.saveGeometry())
         
-        # LineEdit 값들 저장
-        self.settings.setValue('MainWindow/margin', self.basic_tab.margin_edit.text())
-        self.settings.setValue('MainWindow/diff_width', self.basic_tab.diff_width_edit.text())
-        
-        # 투명도 슬라이더 값 저장
-        self.settings.setValue('MainWindow/opacity', self.basic_tab.opacity_slider.value())
-        
-        # 캡처 영역 창 설정 저장
-        if self.cap_region_window:
-            self.settings.setValue('CapRegionWindow/geometry', 
-                                 self.cap_region_window.saveGeometry())
-        
-        # 기타설정값 저장
-        self.settings.setValue('MainWindow/file_name', self.basic_tab.file_name_edit.text())
-        self.settings.setValue('MainWindow/page_loop', self.basic_tab.page_loop_edit.text())
-        self.settings.setValue('MainWindow/capture_delay', self.basic_tab.delay_edit.text())
+        # BasicTab 설정 저장
+        self.basic_tab.saveSettings()
 
     def closeEvent(self, event):
         """프로그램 종료 시 설정 저장"""
         self.saveSettings()
-        if self.cap_region_window:
-            self.cap_region_window.close()
         event.accept()
-
-    def create_cap_region_window(self):
-        """캡처 영역 창을 생성만 하고 보이지 않게 함"""
-        if self.cap_region_window is None:
-            self.cap_region_window = CapRegionWindow(parent=self)
-            
-            # 저장된 투명도 적용
-            opacity = self.basic_tab.opacity_slider.value() / 100.0
-            self.cap_region_window.setWindowOpacity(opacity)
-            
-            # 저장된 캡처 영역 창 설정 불러오기
-            cap_region_geometry = self.settings.value('CapRegionWindow/geometry')
-            if cap_region_geometry:
-                self.cap_region_window.restoreGeometry(cap_region_geometry)
-            else:
-                # 기본 위치와 크기 설정
-                self.cap_region_window.setGeometry(300, 300, 400, 300)
-
-    def show_cap_region_window(self):
-        """캡처 영역 창을 생성하고 보이게 함"""
-        if self.cap_region_window is None:
-            self.create_cap_region_window()
-        self.cap_region_window.show()
-
-    def change_opacity(self, value):
-        if self.cap_region_window is not None:
-            opacity = value / 100.0
-            self.cap_region_window.setWindowOpacity(opacity)
-            self.basic_tab.opacity_label.setText(f'투명도: {value}%')
-            
-    def clear_rectangles(self):
-        if self.cap_region_window is not None:
-            self.cap_region_window.rectangle = None
-            self.cap_region_window.can_draw = True  # 초기화 후 다시 그리기 가능 상태로 변경
-            self.cap_region_window.update()
-            
-    def on_margin_changed(self):
-        """Margin 값이 변경될 때 캡처 영역 창 업데이트"""
-        if self.cap_region_window:
-            self.cap_region_window.update()
-
-    def on_diff_width_changed(self):
-        """Diff Width 값이 변경될 때 캡처 영역 창 업데이트"""
-        if self.cap_region_window:
-            self.cap_region_window.update()
-
-    def toggle_cap_region_window(self):
-        """캡처 영역 창 보이기/숨기기 토글"""
-        if self.cap_region_window is None:
-            self.show_cap_region_window()
-        else:
-            if self.cap_region_window.isVisible():
-                self.cap_region_window.hide()
-            else:
-                self.cap_region_window.show()
-
-    def log_message(self, message):
-        """로그 메시지를 추가하는 메서드"""
-        self.basic_tab.log_text_edit.append(message)  # 메시지를 추가
-        self.basic_tab.log_text_edit.verticalScrollBar().setValue(self.basic_tab.log_text_edit.verticalScrollBar().maximum()) # type: ignore
-
-    def start_process(self):
-        self.cap_region_window.hide() # type: ignore
-        # QThread를 사용하여 auto_pdf_capture를 비동기로 실행
-        self.thread = QThread() # type: ignore
-        self.worker = WorkerCapture(self,
-                             self.basic_tab.file_name_edit.text(), 
-                             int(self.basic_tab.page_loop_edit.text()),
-                             0, 
-                             int(self.settings.value('MainWindow/y1')),
-                             int(self.settings.value('MainWindow/x2')), 
-                             int(self.settings.value('MainWindow/y2')),
-                             int(self.basic_tab.margin_edit.text()), 
-                             int(self.basic_tab.diff_width_edit.text()),
-                             float(self.basic_tab.delay_edit.text()),
-                             self.basic_tab.left_first_check.isChecked())
-        self.worker.moveToThread(self.thread) # type: ignore
-
-        self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        self.thread.finished.connect(lambda: self.basic_tab.btn_start.setText("시작"))
-
-        self.thread.start()
-
-    def stop_process(self):
-        """캡쳐 프로세스를 중지합니다."""
-        if hasattr(self, 'worker'):
-            self.worker.stop()
-            self.log_message("캡쳐를 중지했습니다.")
 
     def create_menu_bar(self):
         """메뉴바 생성 및 설정"""
--- tab_basic.py
+++ tab_basic.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import (QWidget, QPushButton, QVBoxLayout, QSlider,
                            QCheckBox)
 from PyQt5.QtCore import Qt, pyqtSignal, QTimer
 from supa_settings import SupaSettings
+from cap_region_window import CapRegionWindow  # 추가
 
 class BasicTab(QWidget):
     # 파일명이 변경될 때 발생하는 시그널 추가
@@ -12,6 +13,7 @@ class BasicTab(QWidget):
         super().__init__(parent)
         self.main_window = parent
         self.settings = SupaSettings()
+        self.cap_region_window = None
         
         # 파일명 변경 타이머 설정
         self._filename_timer = QTimer(self)
@@ -20,6 +22,161 @@ class BasicTab(QWidget):
         self._filename_timer.timeout.connect(self._emit_filename_changed)
         
         self.setup_ui()
+        self.create_cap_region_window()  # 시작 시 캡처 영역 창 생성
+        
+    def create_cap_region_window(self):
+        """캡처 영역 창을 생성만 하고 보이지 않게 함"""
+        if self.cap_region_window is None:
+            self.cap_region_window = CapRegionWindow(parent=self.main_window)
+            
+            # 저장된 투명도 적용
+            opacity = self.getOpacity() / 100.0
+            self.cap_region_window.setWindowOpacity(opacity)
+            
+            # 저장된 캡처 영역 창 설정 불러오기
+            cap_region_geometry = self.settings.value('CapRegionWindow/geometry')
+            if cap_region_geometry:
+                self.cap_region_window.restoreGeometry(cap_region_geometry)
+            else:
+                # 기본 위치와 크기 설정
+                self.cap_region_window.setGeometry(300, 300, 400, 300)
+
+    def show_cap_region_window(self):
+        """캡처 영역 창을 생성하고 보이게 함"""
+        if self.cap_region_window is None:
+            self.create_cap_region_window()
+        self.cap_region_window.show()
+
+    def hide_cap_region_window(self):
+        """캡처 영역 창을 숨김"""
+        if self.cap_region_window is not None:
+            self.cap_region_window.hide()
+
+    def toggle_cap_region_window(self):
+        """캡처 영역 창 토글"""
+        if self.cap_region_window is None:
+            self.create_cap_region_window()
+            
+        if self.cap_region_window.isVisible():
+            self.hide_cap_region_window()
+            self.toggle_modaless_button.setText("캡쳐영역 가이드")
+        else:
+            self.show_cap_region_window()
+            self.toggle_modaless_button.setText("캡쳐영역 숨기기")
+
+    def clear_rectangles(self):
+        """캡처 영역 창의 사각형을 초기화"""
+        if self.cap_region_window is not None:
+            self.cap_region_window.cap_region_rect = None
+            self.cap_region_window.can_draw = True
+            self.cap_region_window.update()
+
+    def change_opacity(self, value: int) -> None:
+        """캡처 영역 창의 투명도를 변경"""
+        if self.cap_region_window is not None:
+            opacity = value / 100.0
+            self.cap_region_window.setWindowOpacity(opacity)
+
+    def on_left_first_changed(self, state):
+        """좌측부터 스위치 상태가 변경되면 호출되는 메서드"""
+        is_checked = bool(state)
+        self.settings.setValue('basic/left_first', is_checked)
+        print(f"좌측부터 설정이 변경되었습니다: {is_checked}")
+
+    def on_start_button_clicked(self):
+        """시작/중지 버튼 클릭 이벤트 핸들러"""
+        if self.btn_start.text() == "시작":
+            self.btn_start.setText("중지")
+            self.main_window.start_process()
+        else:
+            self.btn_start.setText("시작")
+            self.main_window.stop_process()
+
+    def _on_filename_changed(self, text: str):
+        """파일명이 변경될 때 호출되는 메서드"""
+        # 타이머 재시작
+        self._filename_timer.start()
+        
+    def _emit_filename_changed(self):
+        """타이머가 만료되면 현재 파일명으로 시그널 발생"""
+        current_filename = self.file_name_edit.text()
+        self.filename_changed.emit(current_filename)
+
+    def setOpacity(self, value: int) -> None:
+        """투명도 값을 설정하고 라벨을 업데이트"""
+        self.opacity_slider.setValue(value)
+        self.opacity_label.setText(f'투명도: {value}%')
+        
+        # 캡처 영역 창의 투명도도 함께 업데이트
+        if self.cap_region_window is not None:
+            opacity = value / 100.0
+            self.cap_region_window.setWindowOpacity(opacity)
+
+    def _on_opacity_changed(self, value: int) -> None:
+        """투명도 슬라이더 값이 변경될 때 호출되는 메서드"""
+        self.setOpacity(value)
+
+    def loadSettings(self):
+        """저장된 설정 불러오기"""
+        self.margin_edit.setText(self.settings.value('MainWindow/margin', '0'))
+        self.diff_width_edit.setText(self.settings.value('MainWindow/diff_width', '0'))
+        
+        # 투명도 슬라이더 값 불러오기
+        opacity = int(self.settings.value('MainWindow/opacity', 50))
+        self.setOpacity(opacity)
+        
+        # 기타설정값 불러오기
+        self.file_name_edit.setText(self.settings.value('MainWindow/file_name', ''))
+        self.page_loop_edit.setText(self.settings.value('MainWindow/page_loop', ''))
+        self.delay_edit.setText(self.settings.value('MainWindow/capture_delay', '0'))
+        
+    def saveSettings(self):
+        """현재 설정 저장"""
+        self.settings.setValue('MainWindow/margin', self.margin_edit.text())
+        self.settings.setValue('MainWindow/diff_width', self.diff_width_edit.text())
+        self.settings.setValue('MainWindow/opacity', self.opacity_slider.value())
+        self.settings.setValue('MainWindow/file_name', self.file_name_edit.text())
+        self.settings.setValue('MainWindow/page_loop', self.page_loop_edit.text())
+        self.settings.setValue('MainWindow/capture_delay', self.delay_edit.text())
+        
+        # 캡처 영역 창 설정 저장
+        if self.cap_region_window:
+            self.settings.setValue('CapRegionWindow/geometry', 
+                                 self.cap_region_window.saveGeometry())
+        
+    def getOpacity(self) -> int:
+        """현재 투명도 값 반환"""
+        return self.opacity_slider.value()
+        
+    def getMargin(self) -> str:
+        """현재 margin 값 반환"""
+        return self.margin_edit.text()
+        
+    def getDiffWidth(self) -> str:
+        """현재 diff_width 값 반환"""
+        return self.diff_width_edit.text()
+        
+    def getFileName(self) -> str:
+        """현재 파일명 반환"""
+        return self.file_name_edit.text()
+        
+    def getPageLoop(self) -> str:
+        """현재 page_loop 값 반환"""
+        return self.page_loop_edit.text()
+        
+    def getDelay(self) -> str:
+        """현재 delay 값 반환"""
+        return self.delay_edit.text()
+
+    def on_margin_changed(self):
+        """Margin 값이 변경될 때 캡처 영역 창 업데이트"""
+        if self.cap_region_window:
+            self.cap_region_window.update()
+
+    def on_diff_width_changed(self):
+        """Diff Width 값이 변경될 때 캡처 영역 창 업데이트"""
+        if self.cap_region_window:
+            self.cap_region_window.update()
 
     def setup_ui(self):
         basic_layout = QVBoxLayout(self)
@@ -30,19 +187,20 @@ class BasicTab(QWidget):
         self.opacity_slider = QSlider(Qt.Orientation.Horizontal, self)
         self.opacity_slider.setRange(0, 70)
         self.opacity_slider.setValue(50)
-        self.opacity_slider.valueChanged.connect(lambda: self.main_window.change_opacity(self.opacity_slider.value()))
+        self.opacity_slider.valueChanged.connect(self._on_opacity_changed)
         
         slider_layout.addWidget(self.opacity_label)
         slider_layout.addWidget(self.opacity_slider)
         basic_layout.addLayout(slider_layout)
 
-        # 모달리스 창 보이기/숨기기 토글 버튼
+        # 캡처 영역 가이드 토글 버튼
         self.toggle_modaless_button = QPushButton('캡쳐영역 가이드', self)
-        self.toggle_modaless_button.clicked.connect(self.main_window.toggle_cap_region_window)
+        self.toggle_modaless_button.clicked.connect(self.toggle_cap_region_window)
         basic_layout.addWidget(self.toggle_modaless_button)
         
+        # 캡처 영역 재설정 버튼
         clear_button = QPushButton('캡쳐영역 재설정', self)
-        clear_button.clicked.connect(self.main_window.clear_rectangles)
+        clear_button.clicked.connect(self.clear_rectangles)
         basic_layout.addWidget(clear_button)        
         basic_layout.addSpacing(15)
 
@@ -51,22 +209,22 @@ class BasicTab(QWidget):
         param_layout = QHBoxLayout()
         
         # margin LineEdit
-        margin_layout = QVBoxLayout()
+        margin_layout = QHBoxLayout()
         margin_label = QLabel('Margin', self)
-        margin_label.setToolTip('사각형의 여백을 설정하여 실제 캡쳐 영역을 확장합니다.')
+        margin_label.setToolTip('캡쳐 영역의 여백 크기(px)')
         self.margin_edit = QLineEdit(self)
-        self.margin_edit.setText('0')
-        self.margin_edit.textChanged.connect(self.main_window.on_margin_changed)
+        self.margin_edit.setPlaceholderText('0')
+        self.margin_edit.textChanged.connect(self.on_margin_changed)
         margin_layout.addWidget(margin_label)
         margin_layout.addWidget(self.margin_edit)
         
         # diff_width LineEdit
-        diff_width_layout = QVBoxLayout()
+        diff_width_layout = QHBoxLayout()
         diff_width_label = QLabel('Diff Width', self)
-        diff_width_label.setToolTip('좌우페이지 여백 차이를 보정하여 캡쳐하기 위한 값')
+        diff_width_label.setToolTip('좌우 캡쳐 영역의 차이값(px)')
         self.diff_width_edit = QLineEdit(self)
-        self.diff_width_edit.setText('0')
-        self.diff_width_edit.textChanged.connect(self.main_window.on_diff_width_changed)
+        self.diff_width_edit.setPlaceholderText('0')
+        self.diff_width_edit.textChanged.connect(self.on_diff_width_changed)
         diff_width_layout.addWidget(diff_width_label)
         diff_width_layout.addWidget(self.diff_width_edit)
 
@@ -113,7 +271,7 @@ class BasicTab(QWidget):
         # file_name LineEdit
         name_layout = QVBoxLayout()
         name_label = QLabel('File Name', self)
-        name_label.setToolTip('생성될 pdf 파일명\n예) 실전에서 바로쓰는 Next.js (박수현 역) - 한빛미디어')
+        name_label.setToolTip('생성될 pdf 파일명\n예) 실전에서 바로쓰는 훈민정음 (세종 저) - 조선출판')
         self.file_name_edit = QLineEdit(self)
         # 파일명이 변경될 때 시그널 발생
         self.file_name_edit.textChanged.connect(self._on_filename_changed)
@@ -134,30 +292,5 @@ class BasicTab(QWidget):
         self.log_text_edit.setReadOnly(True)
         self.log_text_edit.setFixedHeight(200)
         basic_layout.addWidget(self.log_text_edit)
-
-    def on_left_first_changed(self, state):
-        """좌측부터 스위치 상태가 변경되면 호출되는 메서드"""
-        is_checked = bool(state)
-        self.settings.setValue('basic/left_first', is_checked)
-        print(f"좌측부터 설정이 변경되었습니다: {is_checked}")
-
-    def on_start_button_clicked(self):
-        """시작/중지 버튼 클릭 이벤트 핸들러"""
-        if self.btn_start.text() == "시작":
-            self.btn_start.setText("중지")
-            self.main_window.start_process()
-        else:
-            self.btn_start.setText("시작")
-            self.main_window.stop_process()
-
-    def _on_filename_changed(self, text: str):
-        """파일명이 변경될 때 호출되는 메서드"""
-        # 타이머 재시작
-        self._filename_timer.start()
-        
-    def _emit_filename_changed(self):
-        """타이머가 만료되면 현재 파일명으로 시그널 발생"""
-        current_filename = self.file_name_edit.text()
-        self.filename_changed.emit(current_filename)
 
 # end of file
